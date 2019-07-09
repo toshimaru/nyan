dd35702e1acc1b4d1239882980565c751ae82cbd
toshimaru <me@toshimaru.net>
2019-07-10 05:41:11 +0900

    Refactoring with spf13/cobra (version flag is implemented)

--- main.go
+++ main.go
@@ -7,18 +7,40 @@ import (
 	"github.com/alecthomas/chroma/formatters"
 	"github.com/alecthomas/chroma/lexers"
 	"github.com/alecthomas/chroma/styles"
+	"github.com/spf13/cobra"
 )
 
-func main() {
-	filename := "README.md"
-	data, err := ioutil.ReadFile(filename)
-	if err != nil {
-		panic("Read Error!")
-	}
+var showVersion bool
 
-	style := styles.Get("swapoff")
-	lexer := lexers.Match(filename)
-	formatter := formatters.Get("terminal256")
-	iterator, _ := lexer.Tokenise(nil, string(data))
-	formatter.Format(os.Stdout, style, iterator)
+var rootCmd = &cobra.Command{
+	Use:     "nyan [FILE]",
+	Short:   "Colorized cat",
+	Long:    "Colorized cat",
+	Example: `$ nyan FILE`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if showVersion {
+			cmd.Println("Version 0.0.0 (not yet released)")
+			return
+		}
+
+		filename := "README.md"
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			panic("Read Error!")
+		}
+		style := styles.Get("swapoff")
+		lexer := lexers.Match(filename)
+		formatter := formatters.Get("terminal256")
+		iterator, _ := lexer.Tokenise(nil, string(data))
+		formatter.Format(os.Stdout, style, iterator)
+	},
 }
+
+func init() {
+	rootCmd.PersistentFlags().BoolVarP(&showVersion, "version", "v", false, `show version`)
+}
+
+func main() {
+	rootCmd.Execute()
+}
+
