337e8b87cad7f07185295f1e9a5e112346add066
toshimaru <me@toshimaru.net>
2019-07-25 06:34:08 +0900

    Test: theme option

--- main_test.go
+++ main_test.go
@@ -43,6 +43,29 @@ func TestExecute(t *testing.T) {
 
 	assert.Nil(t, err)
 	assert.NotNil(t, o.String())
+	assert.Contains(t, o.String(), "[38;5;197mpackage[0m[38;5;231m")
+}
+
+func TestInvalidTheme(t *testing.T) {
+	o := bytes.NewBufferString("")
+	rootCmd.SetArgs([]string{"testdata/dummy.go", "-t", "invalid"})
+	rootCmd.SetOutput(o)
+	err := rootCmd.Execute()
+
+	assert.Nil(t, err)
+	assert.NotNil(t, o.String())
+	assert.Contains(t, o.String(), "[1m[38;5;231mpackage")
+}
+
+func TestValidTheme(t *testing.T) {
+	o := bytes.NewBufferString("")
+	rootCmd.SetArgs([]string{"testdata/dummy.go", "-t", "vim"})
+	rootCmd.SetOutput(o)
+	err := rootCmd.Execute()
+
+	assert.Nil(t, err)
+	assert.NotNil(t, o.String())
+	assert.Contains(t, o.String(), "[38;5;164mpackage[0m")
 }
 
 func TestVersionFlag(t *testing.T) {
