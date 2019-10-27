19b768cd7fe44029819eefa65ef1e2c365ea8448
toshimaru <me@toshimaru.net>
2019-10-27 18:09:12 +0900

    Separate SetOut and SetErr

--- main.go
+++ main.go
@@ -40,7 +40,8 @@ func init() {
 	rootCmd.PersistentFlags().StringVarP(&theme, "theme", "t", "monokai", fmt.Sprintf("Set color theme for syntax highlighting\nAvailable themes: %s", styles.Names()))
 	rootCmd.PersistentFlags().StringVarP(&language, "language", "l", "", "Specify language for syntax highlighting")
 
-	rootCmd.SetOutput(colorable.NewColorableStdout())
+	rootCmd.SetOut(colorable.NewColorableStdout())
+	rootCmd.SetErr(colorable.NewColorableStderr())
 }
 
 func main() {
